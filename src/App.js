876bbbe604e25c7023a64d3e9fd0ae76d1fd94aa
eunbi-kang <ebzzang1905@naver.com>
2024-04-26 13:52:47 +0900

    createBrowserRouter 이용하여 라우터 골격 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
 function App() {
-  return <><h1>안녕하세요?</h1></> 
+  return (
+  <>
+  <Outlet />
+  </> 
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import NotFound from "./pages/NotFound";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import VideoDetail from './pages/VideoDetail';
+import Videos from './pages/Videos';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElment: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> }, // 특정 사용자가 Videos가 아닌
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
