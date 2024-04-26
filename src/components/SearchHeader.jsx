7723d69bb18f54e121e8633eb966fc6b66851a92
eunbi-kang <ebzzang1905@naver.com>
2024-04-26 16:42:54 +0900

    git ignore 추가 + Search Form 만들기

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React from "react";
+import { BsYoutube, BsSearch } from "react-icons/bs";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SearchHeader() {
-  return <header>
-    <div>
+  const navigate = useNavigate();
+  const [isInputClicked, setIsInputClicked] = useState(false);
+  const [text, setText] = useState("");
 
-    </div>
-    <form>
-        
-    </form>
-  </header>
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+  return (
+    <header>
+      <Link to='/'>
+        <BsYoutube />
+        <h1>Youtube</h1>
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onFocus={() => {
+            setIsInputClicked(true);
+          }}
+          onBlur={() => {
+            setIsInputClicked(false);
+          }}
+          placeholder={isInputClicked === true ? "" : "Search..."}
+        />
+        <button>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -5,23 +5,25 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import NotFound from "./pages/NotFound";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import VideoDetail from './pages/VideoDetail';
-import Videos from './pages/Videos';
+import VideoDetail from "./pages/VideoDetail";
+import Videos from "./pages/Videos";
 
-const router = createBrowserRouter([
-  {
-    path: "/youtubi",
-    element: <App />,
-    errorElment: <NotFound />,
-    children: [
-      { index: true, element: <Videos /> },
-      { path: "", element: <Videos /> },
-      { path: "videos", element: <Videos /> },
-      { path: "videos/:keyword", element: <Videos /> },
-      { path: "videos/watch/:videoId", element: <VideoDetail /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElment: <NotFound />,
+      children: [
+        { index: true, element: <Videos /> },
+        { path: "", element: <Videos /> },
+        { path: "videos", element: <Videos /> },
+        { path: "videos/:keyword", element: <Videos /> },
+        { path: "videos/watch/:videoId", element: <VideoDetail /> },
+      ],
+    },
+  ]
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
