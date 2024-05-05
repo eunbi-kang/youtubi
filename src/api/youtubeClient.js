d1c73db7809369bdb3d759240944e0361a21eac5
eunbi-kang <ebzzang1905@naver.com>
2024-05-05 22:29:51 +0900

    test code 업로드

--- src/api/youtubeClient.js
+++ src/api/youtubeClient.js
@@ -1,33 +1,54 @@
 import axios from "axios";
-import youtubeKey from "../constant/youtubeKey";
+import * as youtubeKey from "../constant/youtubeKey";
 
 export default class YoutubeClient {
+
   constructor() {
+    const youtubeApiKey = process.env.REACT_APP_YOUTUBE_API_KEY1;
+    const youtubeApiKeyIndex = 1;
+
+    const apiKey = youtubeKey.activeYoutubeKey;
     /* axios 통신을 할 때, 필요한 기본적인 Setting을 axios.create를 통해 설정해 준 다음
        httpClient에 할당해준다. */
     this.httpClient = axios.create({
         baseURL: 'http://www.googleapis.com/youtube/v3',
-        params: { key: process.env.REACT_APP_YOUTUBE_API_KEY3 }
+        params: { key: process.env.REACT_APP_YOUTUBE_API_KEY4 }
     })
   }
 
+  changeYoutubeApiKey() {
+    const key = "REACT_APP_YOUTUBE_API_KEY";
+    if (this.youtubeApiIndex < 4) {
+      this.youtubeApiKeyIndex = this.youtubeApiKeyIndex++;
+      key = key + this.youtubeApiIndex;
+      // this.youtubeApiKey == process.env[key];
+      console.log(this.youtubeApiKey);
+    } else if (this.youtubeApiIndex === 4) {
+      this.youtubeApiKeyIndex = 1;
+    }
+  }
+
   async search(params) {
-    // return this.httpClient.get('search', params)
-    // .then((response) => {
-    //   console.log(response.status);
-    //   if (response.status === 403) {
-        
-    //   } else if (response.status === 200) {
-    //     console.log(response);
-    //     console.log("succedd");
-    //     // response;
+    // params.params.key = process.env.REACT_APP_YOUTUBE_API_KEY4;
+    // const result = this.httpClient.get('search', params)
+    // .then(res => {
+      
+    //   if (res.status === 403) {
+    //     this.changeYoutubeApiKey();
+    //     this.search(params);   
+    //   } else {
+    //     console.log(res);
+    //     return res;
     //   }
     // });
+    // return result;
+    // params.params.key = process.env.REACT_APP_YOUTUBE_API_KEY4;
     return this.httpClient.get('search', params);
-    //this.httpClient.get('search', params).then(response => response.data);
   }
 
   async videos(params) {
+    // console.log(params);
+    // params.params.key = process.env.REACT_APP_YOUTUBE_API_KEY4;
     return this.httpClient.get('videos', params);
   }
 
--- src/constant/youtubeKey.js
+++ src/constant/youtubeKey.js
@@ -1,3 +1,10 @@
-const youtubeKey = process.env.REACT_APP_YOUTUBE_API_KEY1;
+export const activeYoutubeKey = process.env.REACT_APP_YOUTUBE_API_KEY1;
+export const youtubeKey1 = process.env.REACT_APP_YOUTUBE_API_KEY1;
+export const youtubeKey2 = process.env.REACT_APP_YOUTUBE_API_KEY2;
+export const youtubeKey3 = process.env.REACT_APP_YOUTUBE_API_KEY3;
 
-export default youtubeKey;
+export function updateYoutubeKey(inputKey) {
+    this.youtubeKey = inputKey;
+}
+
+export default activeYoutubeKey;
