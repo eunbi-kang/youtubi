3e5102a1ec91ab15ad3e7c1671cd1b1b74a26c6b
eunbi-kang <ebzzang1905@naver.com>
2024-05-08 17:22:23 +0900

    api key 할당량 사용 초과시 다른키 대체 사용하도록

--- src/api/youtubeClient.js
+++ src/api/youtubeClient.js
@@ -1,58 +1,88 @@
 import axios from "axios";
-import * as youtubeKey from "../constant/youtubeKey";
 
 export default class YoutubeClient {
 
-  constructor() {
-    const youtubeApiKey = process.env.REACT_APP_YOUTUBE_API_KEY1;
-    const youtubeApiKeyIndex = 1;
+  state = {
+    index: 1,
+    key: process.env["REACT_APP_YOUTUBE_API_KEY1"],
+    keyPrefix: "REACT_APP_YOUTUBE_API_KEY"
+  }
 
-    const apiKey = youtubeKey.activeYoutubeKey;
+  constructor() {
     /* axios 통신을 할 때, 필요한 기본적인 Setting을 axios.create를 통해 설정해 준 다음
        httpClient에 할당해준다. */
     this.httpClient = axios.create({
-        baseURL: 'http://www.googleapis.com/youtube/v3',
-        params: { key: process.env.REACT_APP_YOUTUBE_API_KEY4 }
+      // baseURL: 'http://www.googleapis.com/youtube/v3',
+      // params: { key: process.env.REACT_APP_YOUTUBE_API_KEY3 }
+      baseURL: 'http://www.googleapis.com/youtube/v3'
     })
   }
 
-  changeYoutubeApiKey() {
-    const key = "REACT_APP_YOUTUBE_API_KEY";
-    if (this.youtubeApiIndex < 4) {
-      this.youtubeApiKeyIndex = this.youtubeApiKeyIndex++;
-      key = key + this.youtubeApiIndex;
-      // this.youtubeApiKey == process.env[key];
-      console.log(this.youtubeApiKey);
-    } else if (this.youtubeApiIndex === 4) {
-      this.youtubeApiKeyIndex = 1;
+  rotateYoutubeKey() {
+    console.log("rotate executed" + this.state.index);
+    if (this.state.index < 4) {
+      this.state.index = this.state.index + 1;
+      this.state.key = process.env[this.state.keyPrefix + this.state.index];
+      console.log(this.state.key);
+    } else if (this.state.index === 4) {
+      this.state.index = 1;
+      this.state.key = process.env["REACT_APP_YOUTUBE_API_KEY1"];
     }
+    console.log("rotate finishied" + this.state.index);
+    // Key 4개 모두 실패 시 fakeYoutubeClient 호출하도록 변경
   }
 
   async search(params) {
-    // params.params.key = process.env.REACT_APP_YOUTUBE_API_KEY4;
-    // const result = this.httpClient.get('search', params)
-    // .then(res => {
-      
-    //   if (res.status === 403) {
-    //     this.changeYoutubeApiKey();
-    //     this.search(params);   
-    //   } else {
-    //     console.log(res);
-    //     return res;
-    //   }
-    // });
-    // return result;
-    // params.params.key = process.env.REACT_APP_YOUTUBE_API_KEY4;
-    return this.httpClient.get('search', params);
+    params.params.key = this.state.key;
+    const result = this.httpClient.get('search', params)
+      .then((response) => {
+        // console.log(this.state.key);
+        // console.log(response.status);
+        return response;
+      })
+      .catch((err) => {
+        if (err.response.status === 403) {
+          console.log("403");
+          this.rotateYoutubeKey();
+        }
+        console.log(err);
+      });
+    return result;
   }
 
   async videos(params) {
-    // console.log(params);
-    // params.params.key = process.env.REACT_APP_YOUTUBE_API_KEY4;
-    return this.httpClient.get('videos', params);
+    params.params.key = this.state.key;
+    const result = this.httpClient.get('videos', params)
+      .then((response) => {
+        // console.log(this.state.key);
+        // console.log(response.status);
+        return response;
+      })
+      .catch((err) => {
+        if (err.response.status === 403) {
+          console.log("403");
+          this.rotateYoutubeKey();
+        }
+        console.log(err);
+      });
+    return result;
   }
 
   async channels(params) {
-    return this.httpClient.get('channels', params);
+    params.params.key = this.state.key;
+    const result = this.httpClient.get('channels', params)
+      .then((response) => {
+        // console.log(this.state.key);
+        // console.log(response.status);
+        return response;
+      })
+      .catch((err) => {
+        if (err.response.status === 403) {
+          console.log("403");
+          this.rotateYoutubeKey();
+        }
+        console.log(err);
+      });
+    return result;
   }
 }
