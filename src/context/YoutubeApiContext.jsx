fee13f61431b9f382f42474bc19232c78acba9d0
Eunbi-Kang <ebzzang1905@naver.com>
2024-08-20 21:44:38 +0900

    로컬-배포 검색기능 안되는거 주석으로 임시마커표시, 일단 기능 돌아가도록 구현

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -10,10 +10,16 @@ export const YoutubeApiContext = createContext();
 // const client = new FakeYoutubeClient();
 // const client = new YoutubeClient();
 
+
+/* ----- 아래에서 수정 ------ */
 // 배포용
-const client = process.env.NODE_ENV === "development" ? new FakeYoutubeClient() : new YoutubeClient() ;
+// const client = process.env.NODE_ENV === "development" ? new FakeYoutubeClient() : new YoutubeClient() ;
 //테스트용
 // const client = process.env.NODE_ENV === "development" ? new YoutubeClient() : new FakeYoutubeClient() ;
+
+const client = new YoutubeClient(); // 나중에 지워야 할 코드
+
+/* ------ 위에서 수정 ------- */
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
